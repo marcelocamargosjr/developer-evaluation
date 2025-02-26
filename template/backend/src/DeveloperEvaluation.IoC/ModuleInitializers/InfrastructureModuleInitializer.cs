a1619bd237c427c9bad3d049f70219ea40ca0213
Marcelo Camargos <marcelocamargosjr@gmail.com>
2025-02-26 15:07:52 -0300

    :sparkles: feat: added API project developer-evaluation

---
+++ template/backend/src/DeveloperEvaluation.IoC/ModuleInitializers/InfrastructureModuleInitializer.cs
@@ -0,0 +1,18 @@
+﻿using DeveloperEvaluation.Domain.Repositories;
+using DeveloperEvaluation.ORM;
+using DeveloperEvaluation.ORM.Repositories;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DeveloperEvaluation.IoC.ModuleInitializers;
+
+public class InfrastructureModuleInitializer : IModuleInitializer
+{
+    public void Initialize(WebApplicationBuilder builder)
+    {
+        builder.Services.AddScoped<DbContext>(provider => provider.GetRequiredService<DefaultContext>());
+        builder.Services.AddScoped<IUserRepository, UserRepository>();
+    }
+}
---
+++ template/backend/src/DeveloperEvaluation.WebApi/Program.cs
@@ -0,0 +1,84 @@
+using DeveloperEvaluation.Application;
+using DeveloperEvaluation.Common.HealthChecks;
+using DeveloperEvaluation.Common.Logging;
+using DeveloperEvaluation.Common.Security;
+using DeveloperEvaluation.Common.Validation;
+using DeveloperEvaluation.IoC;
+using DeveloperEvaluation.ORM;
+using DeveloperEvaluation.WebApi.Middleware;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+
+namespace DeveloperEvaluation.WebApi;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        try
+        {
+            Log.Information("Starting web application");
+
+            WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+            builder.AddDefaultLogging();
+
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+
+            builder.AddBasicHealthChecks();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<DefaultContext>(options =>
+                options.UseNpgsql(
+                    builder.Configuration.GetConnectionString("DefaultConnection"),
+                    b => b.MigrationsAssembly("DeveloperEvaluation.ORM")
+                )
+            );
+
+            builder.Services.AddJwtAuthentication(builder.Configuration);
+
+            builder.RegisterDependencies();
+
+            builder.Services.AddAutoMapper(typeof(Program).Assembly, typeof(ApplicationLayer).Assembly);
+
+            builder.Services.AddMediatR(cfg =>
+            {
+                cfg.RegisterServicesFromAssemblies(
+                    typeof(ApplicationLayer).Assembly,
+                    typeof(Program).Assembly
+                );
+            });
+
+            builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+
+            var app = builder.Build();
+            app.UseMiddleware<ValidationExceptionMiddleware>();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseBasicHealthChecks();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+        catch (Exception ex)
+        {
+            Log.Fatal(ex, "Application terminated unexpectedly");
+        }
+        finally
+        {
+            Log.CloseAndFlush();
+        }
+    }
+}
